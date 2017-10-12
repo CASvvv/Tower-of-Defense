5360c6c4b6a9a3f6303263d7f277f4494c3e8aad
akira_sato <akiracas4108@gmail.com>
2017-10-12 09:40:42 +0900

    first commit

---
+++ Assets/Scripts/army.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class army : MonoBehaviour {
+
+	public float moveSpeed;
+	private Transform _transform;
+
+	// Use this for initialization
+	void Start () {
+		_transform = this.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_transform.position += _transform.up * moveSpeed;
+	}
+}
---
+++ Assets/Scripts/wall.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wall : MonoBehaviour {
+
+	[SerializeField]
+	public bool rightRotate;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D (Collision2D army) {
+		if (army.gameObject.CompareTag("player")) {
+			army.transform.position += army.transform.up * (-0.1f);
+			if (rightRotate) {
+				army.transform.Rotate (new Vector3 (0, 0, 90));
+			} else {
+				army.transform.Rotate (new Vector3 (0, 0, -90));
+			}
+		}
+	}
+}
